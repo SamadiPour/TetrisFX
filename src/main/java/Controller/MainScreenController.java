195a2cc5c80ea8ffac9f0aeecf3725372d5ad41d
SamadiPour <samadipoor2@gmail.com>
2019-04-21 21:46:39 +0430

    animation for option button

--- src/main/java/Controller/MainScreenController.java
+++ src/main/java/Controller/MainScreenController.java
@@ -2,10 +2,14 @@ package Controller;
 
 import Module.DragHandler;
 import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,18 +31,34 @@ public class MainScreenController implements Initializable {
     private JFXButton optionBtn;
     @FXML
     private AnchorPane topBarNav;
+    @FXML
+    private MaterialDesignIconView optionBtnIcon;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Executors
-                .newSingleThreadExecutor()
-                .submit(() -> DragHandler.makeStageDragable(rootAnchorPane));
         someInitiations();
     }
 
     private void someInitiations() {
+        //add drag
+        Executors
+                .newSingleThreadExecutor()
+                .submit(() -> DragHandler.makeStageDragable(rootAnchorPane));
+
+        //set button action
         exitBtn.setOnMouseReleased(event -> System.exit(0));
         minimizeBtn.setOnMouseReleased(event -> ((Stage) rootAnchorPane.getScene().getWindow()).setIconified(true));
+
+        //rotate animation
+        RotateTransition rotateTransition = new RotateTransition(Duration.millis(1000), optionBtnIcon);
+        rotateTransition.setInterpolator(Interpolator.LINEAR);
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setToAngle(360);
+        rotateTransition.setCycleCount(-1);
+
+        //set rotate on hover
+        optionBtn.setOnMouseEntered(event -> rotateTransition.play());
+        optionBtn.setOnMouseExited(event -> rotateTransition.pause());
     }
 
 
